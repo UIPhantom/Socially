b47b6dfb8b09ae683113b472dd5cf0cb45bd1a3e
kerneloverload <kerneloverload@gmail.com>
2021-11-20 18:00:29 +0530

    tweeking

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,4 +1,3 @@
-const inputs = document.querySelectorAll('.form-signup input');
 const labels = document.querySelectorAll('.form-signup label');
 
 labels.forEach(label => {
@@ -10,19 +9,29 @@ labels.forEach(label => {
 		.join('');
 });
 
-function firstStart(){
+function firstStart() {
 	const main = document.getElementsByTagName("main");
 	const head = document.getElementById("head");
 	const foot = document.getElementById("footer");
 	const input = document.getElementsByTagName("input");
 	const btn = document.getElementsByTagName("button");
 	const label = document.getElementsByTagName("label");
+	const select = document.getElementById("select-gender");
 	foot.classList.add("footer");
 	head.classList.add("header");
-	label[0].style.display = "block";
-	label[1].style.display = "block";
-	btn[0].style.visibility = "visible";
+
+	for (let index = 0; index < label.length - 1; index++) {
+		label[index].style.display = "block";
+	}
+
+	for (let index = 0; index < btn.length; index++) {
+		btn[index].style.visibility = "visible";
+	}
+
 	input[0].type = "email";
 	input[1].type = "password";
+	input[2].type = "text";
+	input[3].type = "text";
+	select.style.display = "block";
 	main[0].classList.add("main");
 }
