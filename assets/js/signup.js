3dfcead5928607a5cd61cc93d1ba85cd876e1962
kerneloverload <kerneloverload@gmail.com>
2021-11-20 17:09:42 +0530

    signup form

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,10 +1,5 @@
-// const input = document.getElementsByTagName("input");
-// input[0].addEventListener("click" , () => {
-//     input[0].style.transition = "border 5s"
-//     input[0].style.borderWidth = "";    
-// });
-const inputs = document.querySelectorAll('.form-signin input');
-const labels = document.querySelectorAll('.form-signin label');
+const inputs = document.querySelectorAll('.form-signup input');
+const labels = document.querySelectorAll('.form-signup label');
 
 labels.forEach(label => {
 	label.innerHTML = label.innerText
