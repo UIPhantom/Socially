698803ebeeb69fcab6ed341abfd37f3659b5f7db
kerneloverload <kerneloverload@gmail.com>
2021-11-27 22:10:43 +0530

    add-new-posts

---
+++ assets/js/wall.js
@@ -0,0 +1,17 @@
+const postContainer = document.getElementById("new-post-main");
+const floatingButton = document.getElementById("floating-btn");
+const floatBtnIcon = document.getElementById("floating-btn-icon");
+let floatBtn = false;
+floatingButton.addEventListener("click", function (){
+     floatBtn = !floatBtn;
+     if(floatBtn){
+        postContainer.style.display = "block";
+        floatBtnIcon.classList.remove("fa-plus-circle");
+        floatBtnIcon.classList.add("fa-arrow-circle-left");
+     }
+     else{
+        postContainer.style.display = "none";
+        floatBtnIcon.classList.add("fa-plus-circle");
+        floatBtnIcon.classList.remove("fa-arrow-circle-left");
+     }
+});
--- controller/wallposts_controller.js
+++ controller/wallposts_controller.js
@@ -1,5 +1,10 @@
 module.exports.view = (req, res) => {
+    const firstName = res.locals.user.firstname;
+    const lastName = res.locals.user.lastname;
+    const email = res.locals.user.email;
     res.render("user/wallposts", {
-        title: "Socially | Posts"
+        title: "Socially | Posts",
+        name: `${firstName} ${lastName}`,
+        email: `${email}`
     });
 }
